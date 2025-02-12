7115b4733ddffabbdd4f1977dd6991b617c1883b
Michele Ambrose <ambrosem@calsaws.org>
2025-02-12 11:12:12 -0800

    Fixed looking up principal ID argument call

--- source/assignments/iam-identitycenter-assignments.py
+++ source/assignments/iam-identitycenter-assignments.py
@@ -249,7 +249,7 @@ def create_assignment_file(permissionSetsArn,repositoryAssignments):
     try:
         for assignment in repositoryAssignments['Assignments']:
             accounts = resolve_targets(assignment)
-            principalId = lookup_principal_id(assignment['PrincipalId'], assignment['PrincipalType'])
+            principalId = lookup_principal_id(assignment['DisplayName'], assignment['PrincipalType'])
             
             for eachAccount in accounts:
                 if eachAccount != managementAccount:
